e6e7e58f80e2b2aaa7bfa44ad3cb737305796538
Fengyi <wei1910396885@gmail.com>
2019-07-17 17:10:48 +0200

    ui

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,24 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::loginBtnClicked(){
+
+}
+
+void MainWindow::exitBtnClicked(){
+    this->close();
+}
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-}
-
-MainWindow::~MainWindow()
-{
-    delete ui;
+    connect(ui->exit, SIGNAL(clicked()),this,SLOT(exitBtnClicked()));
 }
